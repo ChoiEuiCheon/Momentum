ae300982bcee2ae360ec0dd5ae216dcbb5726a74
choieuicheon <choieuicheon@naver.com>
2022-11-02 16:09:03 +0900

    22.11.02 (clock.js 추가, PadStart() 학습)

--- js/greetings.js
+++ js/greetings.js
@@ -8,14 +8,14 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username);
-    paintGreetings(savedUsedrname);
+    localStorage.setItem(USERNAME_KEY,loginInput.value);
+    paintGreetings();
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
 function paintGreetings(){
+    const savedUsedrname = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${savedUsedrname}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,6 +26,6 @@ if(savedUsedrname===null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit",onLoginSubmit)
 }else{
-    paintGreetings(savedUsedrname);
+    paintGreetings();
 }
 console.log(savedUsedrname);
