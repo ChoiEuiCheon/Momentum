43a46becea5fb31f2a09d90e37a4b4ee391db7fa
choieuicheon <choieuicheon@naver.com>
2022-11-15 23:37:23 +0900

    22.11.15 - img_modify

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg","4.jpg"];
 // images 배열에 배경으로 둘 사진 이름 저장
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
